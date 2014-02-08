1b20a334ecb98bc5d9888c5ccb75501b1ad1ef8e
Poweruser <poweruser.rs@hotmail.com>
2014-02-08 02:56:36 +0100

    Correction of bcb93fdc6c: Forgot the solid-neighbours check in the nether

--- src/main/java/de/minetick/antixray/AntiXRay.java
+++ src/main/java/de/minetick/antixray/AntiXRay.java
@@ -282,7 +282,7 @@ public class AntiXRay {
                                 }
                             }
                         } else if(isNether(worldServer)) {
-                            if(blocksToHide[blockID]) {
+                            if(blocksToHide[blockID] && hasOnlySolidBlockNeighbours(chunk, sectionID, x, y, z, 1)) {
                                 buildBuffer[index] = 87; // nether rack
                             }
                         }
