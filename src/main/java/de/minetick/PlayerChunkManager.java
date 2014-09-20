734f893e983d8bd758ec7c7636c59e3665338165
Poweruser <poweruser.rs@hotmail.com>
2014-09-20 08:52:02 +0200

    ChunkSendQueue requeing and sorting Skipped chunks are only requeued once per tick, in case of multiple packets being set, and the queue is sorted before sending/passing to the packetbuilder, when there have been requeued chunks

--- src/main/java/de/minetick/PlayerChunkManager.java
+++ src/main/java/de/minetick/PlayerChunkManager.java
@@ -151,20 +151,20 @@ public class PlayerChunkManager {
                     buff.remove(ccip);
                 }
             }
-            if(buff.generatedChunks > 0 || buff.loadedChunks > 0 || buff.enlistedChunks > 0) {
-                buff.getPlayerChunkSendQueue().sort(entityplayer);
+            PlayerChunkSendQueue chunkQueue = buff.getPlayerChunkSendQueue();
+            int previouslyskipped = chunkQueue.requeuePreviouslySkipped();
+            if(buff.generatedChunks > 0 || buff.loadedChunks > 0 || buff.enlistedChunks > 0 || previouslyskipped > 0) {
+                chunkQueue.sort(entityplayer);
             }
             if(buff.generatedChunks > 0) {
                 this.shuffleList.remove(entityplayer);
                 this.shuffleList.add(entityplayer);
             }
 
-            PlayerChunkSendQueue chunkQueue = buff.getPlayerChunkSendQueue();
+            int skipped = 0;
             for(int w = 0; w < packetsPerTick; w++) {
                 PacketBuilderChunkDataBulk chunkData = new PacketBuilderChunkDataBulk();
-                int skipped = 0;
-                chunkQueue.requeuePreviouslySkipped();
-                while(chunkQueue.hasChunksQueued() && chunkData.size() < PacketPlayOutMapChunkBulk.c() && skipped < 20) {
+                while(chunkQueue.hasChunksQueued() && chunkData.size() < PacketPlayOutMapChunkBulk.c() && skipped < (20 + previouslyskipped)) {
                     ChunkCoordIntPair chunkcoordintpair = chunkQueue.peekFirst();
                     if (chunkcoordintpair != null && chunkQueue.isOnServer(chunkcoordintpair)) {
                         if(this.world.isLoaded(chunkcoordintpair.x << 4, 0, chunkcoordintpair.z << 4)) {
