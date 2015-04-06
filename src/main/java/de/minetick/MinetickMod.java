79b7ca6f667e9fc8957acb2612b414e3aa717fae
Poweruser <poweruser.rs@hotmail.com>
2015-04-06 11:47:21 +0200

    One thread is enough for async writing of nbt files And close the file outputstream in case of an exception as well

--- src/main/java/de/minetick/MinetickMod.java
+++ src/main/java/de/minetick/MinetickMod.java
@@ -70,7 +70,7 @@ public class MinetickMod {
     private TickTimer tickTimerObject;
     private int timerDelay = 45;
     private ScheduledExecutorService timerService = Executors.newScheduledThreadPool(2, new MinetickThreadFactory(Thread.NORM_PRIORITY + 2, "MinetickMod_TimerService"));
-    private ExecutorService nbtFileService = Executors.newCachedThreadPool(new MinetickThreadFactory(Thread.NORM_PRIORITY - 2, "MinetickMod_NBTFileSaver"));
+    private ExecutorService nbtFileService = Executors.newSingleThreadExecutor(new MinetickThreadFactory(Thread.NORM_PRIORITY - 2, "MinetickMod_NBTFileSaver"));
     private ExecutorService worldTickerService = Executors.newCachedThreadPool(new MinetickThreadFactory(Thread.NORM_PRIORITY + 1, "MinetickMod_WorldTicker"));
     private PathSearchThrottlerThread pathSearchThrottler;
     private LockObject worldTickerLock = new LockObject();
@@ -325,11 +325,11 @@ public class MinetickMod {
         }
 
         public Object call() {
+            FileOutputStream fileoutputstream = null;
             try {
                 long start = System.currentTimeMillis();
-                FileOutputStream fileoutputstream = new FileOutputStream(this.file);
+                fileoutputstream = new FileOutputStream(this.file);
                 NBTCompressedStreamTools.a(this.compound, (OutputStream) fileoutputstream);
-                fileoutputstream.close();
                 long duration = System.currentTimeMillis() - start;
                 if(duration > 1000L) {
                     log.info("Saving the file \"" + this.file.getAbsolutePath() + "\" took " + ((float)(duration/100L) / 10.0F) + " seconds.");
@@ -337,6 +337,12 @@ public class MinetickMod {
             } catch (Exception e) {
                 log.error("Error \""+ e.getMessage() +"\" while saving file: " + this.file.getAbsolutePath());
                 e.printStackTrace();
+            } finally {
+                if(fileoutputstream != null) {
+                    try {
+                        fileoutputstream.close();
+                    } catch (IOException e) {}
+                }
             }
             this.compound = null;
             this.file = null;
