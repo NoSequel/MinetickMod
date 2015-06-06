5d00af3ab49180bcb9dc454a60628c482c871b29
Poweruser <poweruser.rs@hotmail.com>
2015-06-06 08:51:05 +0200

    Dont create a new WorldTicker object on every world tick

--- src/main/java/net/minecraft/server/WorldServer.java
+++ src/main/java/net/minecraft/server/WorldServer.java
@@ -26,7 +26,9 @@ import org.bukkit.event.weather.WeatherChangeEvent;
 
 // Poweruser start
 import de.minetick.ChunkGenerationPolicy;
+import de.minetick.WorldTicker;
 import de.minetick.antixray.AntiXRay;
+import de.minetick.profiler.Profiler;
 // Poweruser end
 
 public class WorldServer extends World implements org.bukkit.BlockChangeDelegate {
@@ -56,6 +58,7 @@ public class WorldServer extends World implements org.bukkit.BlockChangeDelegate
     // Poweruser start
     private PriorityQueue<NextTickListEntry> N;
     private ChunkGenerationPolicy chunkGenerationPolicy;
+    private WorldTicker worldTicker;
 
     public void cancelHeavyCalculations(boolean cancel) {
         this.cancelHeavyCalculations = cancel;
@@ -73,6 +76,13 @@ public class WorldServer extends World implements org.bukkit.BlockChangeDelegate
         } else {
             return 0;
         }
+    }
+
+    public WorldTicker getWorldTicker(Profiler profiler) {
+        if(this.worldTicker == null) {
+            this.worldTicker = new WorldTicker(this, profiler);
+        }
+        return this.worldTicker;
     }
     // Poweruser end
 
